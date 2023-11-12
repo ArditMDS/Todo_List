ffb23a60262680b3da05fcd32d07a45fdf770f02
ArditMDS <ardit.mataj@my-digital-school.org>
2023-11-12 14:24:33 +0100

    authetification fonctionnel + routes classiques fini

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,21 +1,85 @@
 var express = require('express');
 var router = express.Router();
 let sqlQuery = require('../mysql/sql')
+const jwt = require("jsonwebtoken");
 
-/* GET tasks listing. */
-router.get('/', function(req, res, next) {
-    sqlQuery("SELECT id, title, due_date, done  FROM todo", (queryError, results) => {
-        if(results.length > 0){
-            res.json(results)
-        } else {
-            res.send("Il n'y a pas de tâches");
-        }
-    })
+//Get pour voir toutes les taches sans exceptions
+//router.get('/', function(req, res, next) {
+//    sqlQuery("SELECT id, title, due_date, done  FROM todo", (queryError, results) => {
+//        if(results.length > 0){
+//            res.json(results)
+//        } else {
+//            res.send("Il n'y a pas de tâches");
+//        }
+//    })
+//});
+function decodeTokenAndGetUserId(req) {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ')[1];
+    try {
+        // Decode the token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Extract and return the user ID
+        const userId = decoded.id;
+        return userId;
+    } catch (error) {
+        // Handle decoding errors (e.g., invalid token)
+        console.error('Error decoding token:', error);
+        return null;
+    }
+}
+
+//Get pour les taches du user connecté
+router.get('/', function (req, res, next) {
+    const tasksPerPage = 2;
+    const currentPage = req.query.page || 1;
+    const searchQuery = req.query.search;
+    const currentUserId = decodeTokenAndGetUserId(req);
+    const isDone = req.query.done;
+    const offsetValue = (currentPage - 1) * tasksPerPage;
+    let conditions = [];
+    if (isDone === "1") {
+        conditions.push("done = 1");
+    }
+    else if (isDone === "0") {
+        conditions.push("done = 0");
+    }
+    if (searchQuery) {
+        conditions.push(`title LIKE '%${searchQuery}%'`);
+    }
+    const whereClause = conditions.length > 0 ? conditions.join(' AND ') : "1=1";
+    const sqlRequest = `SELECT title, done, due_date FROM todo WHERE ${whereClause} AND user_id= ${currentUserId} LIMIT ${tasksPerPage} OFFSET ${offsetValue}`;
+    sqlQuery(sqlRequest, (result) => {
+        const countQuerySql = `SELECT COUNT(*) as count FROM todo WHERE ${whereClause}`;
+        sqlQuery(countQuerySql, (countResult) => {
+            const totalCount = countResult[0].count;
+            const hasPreviousPage = currentPage > 1;
+            const hasNextPage = (currentPage - 1) * tasksPerPage + result.length < totalCount;
+            res.json({
+                tasks: result,
+                currentUserId,
+                pagination: {
+                    totalCount,
+                    currentPage,
+                    tasksPerPage,
+                    offsetValue,
+                    hasPreviousPage,
+                    hasNextPage,
+                },
+            });
+        });
+    });
 });
+
+
+
 //Get par Id
 router.get('/:id', function(req, res, next) {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ')[1];
     let id = parseInt(req.params.id);
-    sqlQuery(`SELECT * FROM todo WHERE id = ${id}`, (results) => {
+    sqlQuery(`SELECT * FROM todo WHERE id = ${id} AND user_id = ${decodeTokenAndGetUserId(req)}`, (results) => {
         if(results.length > 0) {
             res.json(results);
         } else {
@@ -26,8 +90,8 @@ router.get('/:id', function(req, res, next) {
 
 //Ajouter une nouvelle tache
 router.post('/', (req, res) => {
-    const { title, due_date, done, description, user_id } = req.body;
-    const postQuery = `INSERT INTO todo (title, due_date, done, description, user_id) VALUES ("${title}", "${due_date}", "${done}", "${description}", "${user_id}")`;
+    const { title, due_date, done, description } = req.body;
+    const postQuery = `INSERT INTO todo (title, due_date, done, description, user_id) VALUES ("${title}", "${due_date}", "${done}", "${description}", "${decodeTokenAndGetUserId(req)}")`;
     try {
         sqlQuery(postQuery, (result) => {
             if (result.affectedRows === 1) {
@@ -43,5 +107,33 @@ router.post('/', (req, res) => {
         res.send("Error : " + exception)
     }
 });
+
+
+//Update une tache
+router.patch('/:id', function (req, res) {
+    const id = parseInt(req.params.id);
+    const updatedTask = req.body;
+    const updateFields = Object.keys(updatedTask).map(key => `${key} = '${updatedTask[key]}'`);
+    const sql = `UPDATE todo SET ${updateFields.join(', ')} WHERE id = ${id}`;
+    sqlQuery(sql, (result) => {
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Tâche non trouvée' });
+        }
+        res.json(updatedTask);
+    });
+});
+
+//Supprimer une tache
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    sqlQuery(`DELETE FROM todo WHERE id = ${id}`, (result) => {
+        if (result.affectedRows === 1) {
+            res.json({ message: 'La tâche à été supprimé' });
+        } else {
+            res.status(500).json({ message: 'Erreur lors de la suppression de la tâche' });
+        }
+    });
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,102 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require("mysql2");
+const sqlQuery = require("../mysql/sql");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+function generateToken(id) {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {expiresIn: '1d'});
+}
+
+//  Nouvelle utilisateur
+router.post('/signup', (req, res) => {
+  const body = req.body;
+  if (!body.email || !body.password || !body.display_name) {
+    res.status(400)
+    res.send("Tous les champs sont obligatoires")
+    return
+  }
+
+  if (body.password.length < 8) {
+    res.status(400)
+    res.send("MDP doit avoir au moins 8 symboles")
+    return
+  }
+
+  bcrypt.hash(body.password, 12).then(hashedPassword => {
+    const insertQuery = `INSERT INTO user (email, password, display_name) VALUES ("${body.email}", "${hashedPassword}", "${body.display_name}")`;
+    try {
+      sqlQuery(insertQuery, (result) => {
+        res.status(201)
+        res.send("Utilisateur créé: " + body.email)
+      });
+    } catch (exception) {
+      res.status(500)
+      res.send("Erreur lors de la création : " + exception)
+    }
+  })
+})
+
+router.post('/login', (req, res) => {
+  const body = req.body;
+
+  if (!body.email || !body.password) {
+    res.status(400)
+    res.send("Tous les champs sont obligatoires")
+    return
+  }
+
+  sqlQuery(`SELECT * FROM user WHERE email="${body.email}"`, (results) => {
+    if (results.length === 0) {
+      res.status(400);
+      res.send("Invalid password or email");
+    }
+
+    const user = results[0];
+    bcrypt.compare(body.password, user.password).then(isOk => {
+      if (!isOk) {
+        res.status(400);
+        res.status("Invalid password or email");
+      } else {
+        delete user.password
+        //Generate a JWT Token
+        return res.json({
+          'token': generateToken(user.id),
+          'user': user,
+        })
+      }
+    })
+  });
+})
+router.get('/token-test', (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith('Bearer')) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if(err) {
+        res.status(401)
+        if(err.name == "TokenExpiredError") {
+          res.send('Token éxpiré')
+        } else {
+          res.send("Token invalide")
+        }
+        return
+      }
+      sqlQuery(`SELECT * FROM user WHERE user.id = ${decoded.id}`, result => {
+        if (!result.length) {
+          res.status(401)
+          res.send("Votre email ou votre mot de passe sont incorrects")
+        }
+        const user = result[0]
+        res.send("Vous êtes connecté, bienvenue " + user.display_name)
+      })
+    })
+
+  } else {
+    res.status(401)
+    res.send("Accés refusé")
+  }
+})
 
 module.exports = router;
